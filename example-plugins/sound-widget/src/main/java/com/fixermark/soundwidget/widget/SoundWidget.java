0c660398838211f8877fc092367507cf7b21cff8
Mark T. Tomczak <iam@fixermark.com>
2022-08-29 09:07:00 -0400

    Sound widget now plays sound at absolute path. Should change to configurable path.

--- example-plugins/sound-widget/src/main/java/com/fixermark/soundwidget/widget/SoundWidget.java
+++ example-plugins/sound-widget/src/main/java/com/fixermark/soundwidget/widget/SoundWidget.java
@@ -1,5 +1,7 @@
 package com.fixermark.soundwidget.widget;
 
+import java.io.File;
+
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
@@ -8,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 
 @Description(
@@ -21,12 +25,17 @@ public final class SoundWidget extends SimpleAnnotatedWidget<Boolean> {
   @FXML
   private Pane root;
 
+  private MediaPlayer player;
+
   @FXML
   private void initialize() {
     root.backgroundProperty().bind(
       Bindings.createObjectBinding(
         () -> createSolidColorBackground(getColor()), 
     dataProperty()));
+    dataProperty().addListener((newValue) -> checkSoundPlay());
+    Media sound = new Media("file:///home/mtomczak/alert.wav");
+    player = new MediaPlayer(sound);
   }
 
   private Background createSolidColorBackground(Color color) {
@@ -43,6 +52,15 @@ public final class SoundWidget extends SimpleAnnotatedWidget<Boolean> {
       return Color.LAWNGREEN;
     } else {
       return Color.DARKRED;
+    }
+  }
+
+  private void checkSoundPlay() {
+    final Boolean data = getData();
+
+    if (data.booleanValue()) {
+      player.stop();
+      player.play();
     }
   }
 
